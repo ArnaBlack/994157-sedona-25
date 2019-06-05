7e4966758de3344475f51512d457fd3a50da27ac
ArnaBlack <48154833+ArnaBlack@users.noreply.github.com>
2019-06-05 19:08:23 +0300

    исправление бага с картой

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 'use strict';
 
 var form = document.querySelector('.search-form');
+var formWrap = document.querySelector('.form-wrap');
 var showFormButton = document.querySelector('.btn-search');
 
 showFormButton.addEventListener('click', function (evt) {
  evt.preventDefault();
  form.classList.toggle('search-form-show');
+ formWrap.classList.toggle('form-wrap-show');
 });
